a70fe2fbed1b6422f3596307c8ffb54ef0d67d90
Naowas Morshed Eimon <naowas.morshed.eimon@gmail.com>
2022-01-16 16:12:43 +0600

    migration,seed,model,resource configured

---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Image;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ImageFactory extends Factory
+{
+
+    protected $model = Image::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'path' => 'image.png'
+        ];
+    }
+}
---
+++ database/factories/OfficeFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Office;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OfficeFactory extends Factory
+{
+    protected $model = Office::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'latitude' => $this->faker->latitude,
+            'longitude' => $this->faker->longitude,
+            'address_line1' => $this->faker->address,
+            'approval_status' => 2,
+            'hidden' => false,
+            'price_per_day' => $this->faker->numberBetween(1_000, 2_000),
+            'monthly_discount' => $this->faker->numberBetween(1_0,6_0),
+        ];
+    }
+}
